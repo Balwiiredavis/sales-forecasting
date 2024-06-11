24a24c8976bfc219377a09a7ef4bae8e8e4c2b1e
Balwiiredavis <141266606+Balwiiredavis@users.noreply.github.com>
2024-06-11 18:01:52 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,6 @@
+projects: Store sales forecasting 
+        :Wallmart sales forecasting and time series 
+
+Data source:https://www.kaggle.com/datasets/tanayatipre/store-sales-forecasting-dataset
+           :https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast
+
